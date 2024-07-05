e0848a01c88bea0fd65e4bd835a728f0b777d697
Ahmedsalem001 <99257541+Ahmedsalem001@users.noreply.github.com>
2024-07-06 01:09:45 +0300

    ref error fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,13 +5,16 @@ import searchIcon from "@/public/searchIcon.svg"
 import {pacifico} from "../app/fonts"
 import { useRef } from "react"
 const Hero = () => {
-  const inputRef = useRef();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setFoces = () => {
+    if (inputRef.current) inputRef.current.focus();
+  }
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center ">
       <Image src={hero} alt="hero" fill className="-z-10" />
       <h1  className={`${pacifico.className} text-6xl text-center leading-relaxed text-white mb-12 mt-28`}>“There Is No Friend As <br /> Loyal As A Book.”</h1>
       <p className="text-xl text-white ">“The more that you read, the more things you will know. The more that you learn.”</p>
-      <div   className="flex justify-between  rounded-3xl overflow-hidden items-center px-10 py-1 cursor-pointer mt-24 bg-[#FFFFFFA6] w-[70%] h-20" id="search" onClick={() => {inputRef.current.focus()}}>
+      <div onClick={setFoces} className="flex justify-between  rounded-3xl overflow-hidden items-center px-10 py-1 cursor-pointer mt-24 bg-[#FFFFFFA6] w-[70%] h-20" id="search">
         <input ref={inputRef} name="searsh" type="search" placeholder="search for the book you want..." className="placeholder:text-[#756AB6] pl-2 focus:outline-none bg-transparent w-full h-16
       "/>
         <Image src={searchIcon} alt="search" width={50} height={50}/>
